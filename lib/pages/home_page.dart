9a8c6177a4920a2a25675f01ebdf5204d09cbac8
Edson Bueno <edsonbueno@tokenlab.com.br>
2020-01-11 15:48:31 -0300

    Add detailed instructions on HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -29,15 +29,26 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     final currentFlow = appFlows[_currentBarIndex];
 
+    // We're preventing the root navigator from popping and closing the
+    // app if the inner navigator can handle it, which occurs when it has
+    // more than one screen on it's stack. You can comment the onWillPop
+    // callback and watch it happening.
     return WillPopScope(
-      // We're preventing the root navigator from popping and closing the
-      // app if the inner navigator can handle it, which occurs when it has
-      // more than one screen on it's stack.
       onWillPop: () async =>
           !await currentFlow.navigatorKey.currentState.maybePop(),
       child: Scaffold(
         body: Navigator(
+          // The key in necessary for two reasons:
+          // 1 - For the framework to understand that we're replacing the
+          // navigator even though it's type and location in the tree is
+          // the same. For this isolate purpose a simple ValueKey would fit.
+          // 2 - Being able to access the Navigator's state inside the onWillPop
+          // callback and for emptying it's stack when a tab is re-selected.
+          // That is why a GlobalKey is needed instead of a simple ValueKey.
           key: currentFlow.navigatorKey,
+          // Since this isn't the purpose of this sample, we're not using named
+          // routes, because of that the onGenerateRoute callback will be
+          // called only for the initial route.
           onGenerateRoute: (settings) => MaterialPageRoute(
             settings: settings,
             builder: (context) => IndexedPage(
