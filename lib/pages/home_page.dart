4e7935cc27014ea4199cad5df3f98a8cdd3a090e
Edson Bueno <edsonbueno@tokenlab.com.br>
2020-01-13 09:35:15 -0300

    Change HomePage in order to use the AdaptiveBottomNavigationScaffold widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
-import 'package:adaptive_bottom_nav_sample/app_flow.dart';
-import 'package:adaptive_bottom_nav_sample/indexed_page_flow.dart';
+import 'package:adaptive_bottom_nav_sample/custom/adaptive_bottom_navigation_scaffold.dart';
+import 'package:adaptive_bottom_nav_sample/custom/app_flow.dart';
+import 'package:adaptive_bottom_nav_sample/custom/bottom_navigation_bar_item.dart';
+import 'package:adaptive_bottom_nav_sample/pages/indexed_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -9,65 +12,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _currentBarIndex = 0;
   final List<AppFlow> appFlows = [
     AppFlow(
       title: 'Video',
       iconData: Icons.ondemand_video,
       mainColor: Colors.red,
-      navigatorKey: GlobalKey(),
+      navigatorKey: GlobalKey<NavigatorState>(),
     ),
     AppFlow(
       title: 'Music',
       iconData: Icons.music_note,
       mainColor: Colors.green,
-      navigatorKey: GlobalKey(),
+      navigatorKey: GlobalKey<NavigatorState>(),
     )
   ];
 
   @override
-  Widget build(BuildContext context) {
-    final currentFlow = appFlows[_currentBarIndex];
-
-    // We're preventing the root navigator from popping and closing the
-    // app if the inner navigator can handle it, which occurs when it has
-    // more than one screen on it's stack. You can comment the onWillPop
-    // callback and watch it happening.
-    return WillPopScope(
-      onWillPop: () async =>
-          !await currentFlow.navigatorKey.currentState.maybePop(),
-      child: Scaffold(
-        body: IndexedStack(
-          index: _currentBarIndex,
-          children: [
-            IndexedPageFlow(flow: appFlows[0]),
-            IndexedPageFlow(flow: appFlows[1])
-          ],
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentBarIndex,
-          items: appFlows
-              .map(
-                (flow) => BottomNavigationBarItem(
+  Widget build(BuildContext context) => AdaptiveBottomNavigationScaffold(
+        navigationBarItems: appFlows
+            .map(
+              (flow) => NavigationBarItem(
+                bottomNavigationBarItem: BottomNavigationBarItem(
                   title: Text(flow.title),
                   icon: Icon(flow.iconData),
                 ),
-              )
-              .toList(),
-          onTap: (newIndex) => setState(
-            () {
-              if (_currentBarIndex != newIndex) {
-                _currentBarIndex = newIndex;
-              } else {
-                // If the user is re-selecting the tab, the common
-                // behavior is to empty the stack.
-                currentFlow.navigatorKey.currentState
-                    .popUntil((route) => route.isFirst);
-              }
-            },
-          ),
-        ),
-      ),
-    );
-  }
+                navigatorKey: flow.navigatorKey,
+              ),
+            )
+            .toList(),
+        initialPageBuilder: (flowIndex) {
+          final appFlow = appFlows[flowIndex];
+          return IndexedPage(
+            index: 1,
+            backgroundColor: appFlow.mainColor,
+            containingFlowTitle: appFlow.title,
+          );
+        },
+      );
 }
