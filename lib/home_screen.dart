7373f5e4dea18b0ae0259ded13805a04f14ce05d
Edson Bueno <edsonbueno@tokenlab.com.br>
2020-01-15 05:54:21 -0300

    Change HomePage name to HomeScreen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,17 +1,17 @@
 import 'package:adaptive_bottom_nav_sample/custom/adaptive_bottom_navigation_scaffold.dart';
 import 'package:adaptive_bottom_nav_sample/custom/app_flow.dart';
 import 'package:adaptive_bottom_nav_sample/custom/bottom_navigation_tab.dart';
-import 'package:adaptive_bottom_nav_sample/pages/indexed_page.dart';
+import 'package:adaptive_bottom_nav_sample/indexed_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class HomePage extends StatefulWidget {
+class HomeScreen extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomeScreenState extends State<HomeScreen> {
   final List<AppFlow> appFlows = [
     AppFlow(
       title: 'Video',
