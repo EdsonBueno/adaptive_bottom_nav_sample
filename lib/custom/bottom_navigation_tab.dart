ce63f20a2cb40177b6d7838769281c2eb7b58720
Edson Bueno <edsonbueno@tokenlab.com.br>
2020-01-17 06:41:45 -0300

    Fix BottomNavigationTab's indentation.

--- lib/custom/bottom_navigation_tab.dart
+++ lib/custom/bottom_navigation_tab.dart
@@ -3,9 +3,10 @@ import 'package:flutter/widgets.dart';
 /// Contains the necessary parameters for building either a
 /// [MaterialBottomNavigationScaffold] or [CupertinoBottomNavigationScaffold].
 class BottomNavigationTab {
-  const BottomNavigationTab(
-      {@required this.bottomNavigationBarItem, @required this.navigatorKey})
-      : assert(bottomNavigationBarItem != null),
+  const BottomNavigationTab({
+    @required this.bottomNavigationBarItem,
+    @required this.navigatorKey,
+  })  : assert(bottomNavigationBarItem != null),
         assert(navigatorKey != null);
 
   final BottomNavigationBarItem bottomNavigationBarItem;
